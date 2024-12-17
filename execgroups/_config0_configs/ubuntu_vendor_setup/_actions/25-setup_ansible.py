57655b59009eb4c73ce3483ac03849fb732b365a
gary <gary>
2024-12-17 11:32:12 +0800

    rendered and updated 12-17-2024-11-32

---
+++ execgroups/_config0_configs/ubuntu_vendor_setup/_actions/25-setup_ansible.py
@@ -0,0 +1,7 @@
+def default():
+
+    task = {'method': 'shelloutconfig',
+            'metadata': {'env_vars': [],
+                         'shelloutconfigs': ['config0-publish:::mongodb::create_ansible_replica_hosts']}}
+
+    return task
