9ed123bffb5ea67b73a37aeaaf8faeef729ae039
gary <gary>
2025-02-08 09:22:16 +0800

    rendered and updated 02-08-2025-09-22

---
+++ execgroups/_config0_configs/ubuntu_vendor_init_replica/_actions/30-exec_ansible.py
@@ -0,0 +1,7 @@
+def default():
+
+    task = {'method': 'shelloutconfig',
+            'metadata': {'env_vars': [],
+                         'shelloutconfigs': ['config0-publish:::ansible::resource_wrapper']}}
+
+    return task
---
+++ execgroups/_config0_configs/ubuntu_vendor_setup/_rules/order.py
@@ -0,0 +1,4 @@
+def default():
+
+    return ["15-add_ansible_sshkey.py,end",
+            "25-setup_ansible.py,end"]
